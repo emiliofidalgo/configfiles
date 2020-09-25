be48aee8352cc997f4bdf25610c501d462879091
Emilio Garcia-Fidalgo <emilio.garcia@uib.es>
2020-09-25 12:53:29 +0200

    Do not install vimrc files

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,7 +7,7 @@ echo "Creating new symbolic links..."
 ln -sfv $DIR/.bashrc $HOME/.bashrc
 ln -sfv $DIR/.bash_aliases $HOME/.bash_aliases
 #ln -sfv $DIR/.ssh $HOME/.ssh
-ln -sfv $DIR/.vimrc $HOME/.vimrc
+#ln -sfv $DIR/.vimrc $HOME/.vimrc
 ln -sfv $DIR/.gitconfig $HOME/.gitconfig
 #ln -sfv $DIR/.gitignore_global $HOME/.gitignore_global
 # Link all scripts in .local/bin
